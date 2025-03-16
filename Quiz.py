323c7f9a26bb2199c8f911613c231a18f9e89ca8
quentinrees-francis <quentinrees-francis@cromwell.school.nz>
2025-03-17 11:09:26 +1300

    Quiz.py_2
    - Coded the age check for upper and lower bounds

--- Quiz.py
+++ Quiz.py
@@ -7,14 +7,16 @@ questions  = [
   ""
 ]
 
-easygui.msgbox("Welcome to the NZ Trivia Game")
+easygui.msgbox("Welcome to the Maori Trivia Game!")
 name = easygui.enterbox("What is your name?")
-easygui.msgbox("Welcome to the NZ Trivia Game" + name)
+easygui.msgbox(f"Welcome to the Maori Trivia Game {name}")
 age = easygui.integerbox("How old are you?")
-if age < MIN_AGE:
-  easygui.msgbox("You are too young to play!")
-if age > MAX_AGE:
-  easygui.msgbox("You are too old to play!")
+if age < MAX_AGE and age > MIN_AGE:
+  easygui.msgbox("You are old enough to play.")
 else:
-  easygui.msgbox("You are the right age to play!")
-  easygui.msgbox("You will be asked a series of multiple choice questions. If you get a question correct, you will gain 1 point. If you get a question wrong, you will get one more chance to select the correct answer. If you fail this second chance, you will lose a point. You will have a total of 10 questions.")
+  easygui.msgbox("You are not in the age range to play!")
+  quit()
+easygui.msgbox("Great, lets get started")
+      
+easygui.msgbox("You will be asked a series of multiple choice questions. If you get a question correct, you will gain 1 point. If you get a question wrong, you will get one more chance to select the correct answer. If you fail this second chance, you will lose a point. You will have a total of 10 questions.")
+      
