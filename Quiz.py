ba25ede427fbf802e51dfc04c6e6bc4a88345fcb
quentinrees-francis <quentinrees-francis@cromwell.school.nz>
2025-03-20 13:19:27 +1300

    Update Quiz.py
    - Added code for questions
    - Added code for giving user a second try

--- Quiz.py
+++ Quiz.py
@@ -15,49 +15,63 @@ questions  = [
   "Where is Te Wairere?",
   "Where would you commonly find a ‘manu’?"]
 
-answers = [["a) runga, b) raro, c) roto, d) waho"], 
-  [" a) book, b) table, c) clock, d) wall"] , 
-  ["a) kikorangi, b) karaka, c) pango, d) whero"], 
-  ["a) rohe, b) whenua, c) moutere, d) iwi"], 
-  [" a) long, b) wide, c) small, d) big"], 
-  [" a)kaha, b) manaaki, c) rawe, d) pai"], 
-  [" a) Alexandra, b) Clyde, c) Bannockburn, d) Cromwell"], 
-[" a) sky, b) pre-school, c) swimming pool, d) classroom"]]
+answers = [["a) runga", "b) raro", "c) roto", "d) waho"], 
+["a) book", "b) table", "c) clock", "d) wall"] , 
+["a) kikorangi" , "b) karaka", "c) pango", "d) whero"], 
+["a) rohe" , "b) whenua" , "c) moutere", "d) iwi"], 
+[" a) long", "b) wide", "c) small", "d) big"], 
+[" a)kaha", "b) manaaki", "c) rawe", "d) pai"], 
+[" a) Alexandra", "b) Clyde", "c) Bannockburn", "d) Cromwell"], 
+[" a) sky", "b) pre-school", "c) swimming pool", "d) classroom"]]
 
 correct_answers = [ 
-"b",
-"a",
-"b",
-"c", 
-"d",
-"c",
-"d",
-"a"]
+"b) raro",
+"a) book",
+"b) karaka",
+"c) moutere", 
+"d) big",
+"c) rawe",
+"d) Cromwell",
+"a) sky"]
 
 easygui.msgbox("Welcome to the Maori Trivia Game!" , TITLE)
 name = easygui.enterbox("What is your name?" )
 easygui.msgbox(f"Welcome to the Maori Trivia Game {name}" , TITLE)
-age = easygui.integerbox("How old are you?")
+age = easygui.integerbox("How old are you?" , TITLE)
 if age < MAX_AGE and age > MIN_AGE:
-  easygui.msgbox("You are in the right age range to play.")
+  easygui.msgbox("You are in the right age range to play." , TITLE)
 elif age > MAX_AGE:
-  easygui.msgbox("You are too old to play.")
+  easygui.msgbox("You are too old to play." , TITLE)
   quit()
 elif age < MIN_AGE:
-  easygui.msgbox("You are too young to play.")
+  easygui.msgbox("You are too young to play." , TITLE)
   quit()
       
-easygui.msgbox("You will be asked a series of multiple choice questions. If you get a question correct, you will gain 1 point. If you get a question wrong, you will get one more chance to select the correct answer. If you fail this second chance, you will lose a point. You will have a total of 10 questions.")
+easygui.msgbox("You will be asked a series of multiple choice questions. If you get a question correct, you will gain 1 point. If you get a question wrong, you will get one more chance to select the correct answer. If you fail this second chance, you will lose a point. You will have a total of 10 questions." , TITLE)
 
-PLAYER_READY = easygui.buttonbox(f"Are you ready to begin, {name}?" , choices = ["Yes" , "No"])
+PLAYER_READY = easygui.buttonbox(f"Are you ready to begin, {name}?" , TITLE, choices = ["Yes" , "No"])
 if PLAYER_READY == "Yes":
-  easygui.msgbox("Great, lets get started")
+  easygui.msgbox("Great, lets get started" , TITLE)
 elif PLAYER_READY == "No":
-  easygui.msgbox("Okay, come back later.")
+  easygui.msgbox("Okay, come back later." , TITLE)
   quit()
   
-
-easygui.msgbox(f"Question {q_num}")
+easygui.msgbox(f"Question {q_num}" , TITLE)
 for i in range(len(questions)):
-  answer = easygui.buttonbox(questions[i], choices = answers[i])
+  user_answer = easygui.buttonbox(questions[i], TITLE, choices = answers[i] ,)
+  if user_answer == correct_answers[i]:
+    score += 1
+    easygui.msgbox(f"Correct! Your score is {score}" , TITLE)
+    q_num += 1
+  else:
+      easygui.msgbox("Incorrect. Have another go!" , TITLE)
+      user_answer = easygui.buttonbox(questions[i], TITLE, choices = answers[i] ,)
+      if user_answer == correct_answers[i]:
+        score += 1
+        easygui.msgbox(f"Correct! Your score is {score}" , TITLE)
+        q_num += 1
+      else:
+        easygui.msgbox(f"Incorrect. The correct answer was {correct_answers[i]}. Your score is {score}" , TITLE)
+        q_num += 1
+
 
